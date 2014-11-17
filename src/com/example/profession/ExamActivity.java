2acb52a76964e07670ad7fc566730ac20e788046
eggordeev <egordeev18@gmail.com>
2014-11-17 21:02:59 +0400

    initial commit

---
+++ src/com/example/profession/ExamActivity.java
@@ -0,0 +1,16 @@
+package com.example.profession;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by egordeev on 16.11.14.
+ */
+public class ExamActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exam);
+
+    }
+}
---
+++ src/com/example/profession/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.profession;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        ImageView imageViewProfession1 = (ImageView)findViewById(R.id.imageViewProfession1);
+        ImageView imageViewProfession2 = (ImageView)findViewById(R.id.imageViewProfession2);
+
+        imageViewProfession1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), VerificationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imageViewProfession2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), VerificationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+
+                startActivity(intent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
